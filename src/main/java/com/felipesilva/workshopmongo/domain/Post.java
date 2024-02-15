7ea821970b108bb8616da96abeaf1568bf058e0d
Felipe Silva <joaofelipems21@gmail.com>
2024-02-15 14:16:47 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/felipesilva/workshopmongo/domain/Post.java
+++ src/main/java/com/felipesilva/workshopmongo/domain/Post.java
@@ -1,10 +1,13 @@
 package com.felipesilva.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import com.felipesilva.workshopmongo.dto.AuthorDTO;
+import com.felipesilva.workshopmongo.dto.CommentDTO;
 
 public class Post implements Serializable {
 
@@ -15,6 +18,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -66,6 +71,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
